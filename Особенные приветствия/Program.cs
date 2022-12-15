14a414770f97ed7331cf34368fbd5f85371d7830
vasiltsov <vnigga@list.ru>
2022-12-15 19:49:58 +0300

    Сделал особое привествие

---
+++ Особенные приветствия/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter your name ");
+string username = Console.ReadLine();
+if (username.ToLower() == "nikita") 
+{ Console.Write("Glad to see you, ");
+Console.Write(username);
+}
+else 
+{ Console.Write("Hello, ");
+Console.Write(username);}
