556cff27b19a0ae9107ebdb03c840917e99c8bb6
vasiltsov <vnigga@list.ru>
2022-12-18 17:50:14 +0300

    день по номеру и проверка на квадрат

---
+++ Вычисление квадрата/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Number");
+int number = int.Parse(Console.ReadLine()); 
+//метод, который позволяет полученный параметр преобразовать в число
+double result = Math.Pow(number,2);
+//Math - класс. Pow - Метод этого класса. Принимает два аргумента. number и степень.
+System.Console.WriteLine($"Квадрат от числа {number} равен {result}");
+//$ перед ковычками позволяет использовать фигурные скобки и передавать через них данные из другого кода.
---
+++ Проверка на квадрат/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input A");
+int InputA = int.Parse(Console.ReadLine());
+Console.WriteLine("Input B");
+int InputB = int.Parse(Console.ReadLine());
+if (InputA == InputB*InputB)
+{ Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{ Console.WriteLine("Первое число не является квадратом второго");
+}
