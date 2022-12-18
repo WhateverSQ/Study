84407585aeaea9b18090a6738ab198b11409f2b7
vasiltsov <vnigga@list.ru>
2022-12-18 18:51:11 +0300

    Последняя цифра и -n до n

---
+++ От -n до n/Program.cs
@@ -0,0 +1,7 @@
+﻿int n = int.Parse(Console.ReadLine());
+int count = (-n);
+while(count <= n)
+{
+    Console.Write($"{count},");
+    count = count + 1;
+    }
---
+++ Последняя цифра из числа/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter 3-digit number");
+if (int.TryParse(Console.ReadLine(),out int n))
+        if (n >= 100 && n < 1000)
+    {
+           int x = n % 10;
+           Console.Write($"Last number is {x}");
+    }
+    else {
+        System.Console.WriteLine("NOT A 3-DIGIT NUMBER");
+    }
+   else
+   {
+    System.Console.WriteLine("NOT A NUMBER");
+   }
