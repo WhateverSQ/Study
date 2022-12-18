66dac02aae24d9b3037f1e1a56283373998afe76
vasiltsov <vnigga@list.ru>
2022-12-18 22:20:57 +0300

    вычисление большего и меньшего

---
+++ Homework-1/Task-2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Number A");
+int a = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Number B");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+int min = a;
+if(b > max) 
+{
+max = b;
+}
+else
+{
+min = b;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
