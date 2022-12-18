292701ef04f3adb57af935dfaaf0cc7d2cac1e06
vasiltsov <vnigga@list.ru>
2022-12-18 22:36:28 +0300

    Task 6. Even/Odd

---
+++ Homework-1/Task 6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine()); 
+if (number % 2 == 0)
+{
+    Console.Write("Number is even");
+}
+else 
+{
+    Console.Write("Number is odd");
+}
