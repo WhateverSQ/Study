e3027a07ffcd5fc7d7bfbd7a312239d3634fe92e
vasiltsov <vnigga@list.ru>
2022-12-22 01:59:02 +0300

    Task 10

---
+++ Homework-2/Task 10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number from 100 to 999: ");
+int number = int.Parse(Console.ReadLine());
+if (number >= 100 && number <=999)
+{
+    int Second = (number / 10) % 10;
+    Console.WriteLine($"Second number is {Second}");
+}
+else 
+{
+    Console.WriteLine("Invalid number");
+}
