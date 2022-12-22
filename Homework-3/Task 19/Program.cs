8868b855ca912010e8146d803651308520e7c1f4
vasiltsov <vnigga@list.ru>
2022-12-22 09:00:28 +0300

    Homework-3 fix

--- Homework-3/Task 19/Program.cs
+++ Homework-3/Task 19/Program.cs
@@ -4,10 +4,8 @@
 
 Console.Write("Введите 5-и значное число: ");
 string number = Console.ReadLine();
-
   if (number[0]==number[4] && number[1]==number[3])
   {
     Console.WriteLine($"Число {number} палиндром.");
   }
   else Console.WriteLine($"Число {number} не палиндром.");
-
